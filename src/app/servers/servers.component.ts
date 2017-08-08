ca1b67246d1853aaca152a875ba3b5ca6ac3c2c8
ncepz <septi.nugrahini@gmail.com>
2017-08-08 11:08:46 +0700

    lanjutan lecture 22

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
     //selector :'[app-servers]',
     //selector : '.app-servers',
     selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+ // template: `<app-server></app-server><app-server></app-server>`,
+    templateUrl: './servers.component.html',
     styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
